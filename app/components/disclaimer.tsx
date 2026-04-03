d45cc8e010a67678702ae1a2fedf1b4ce3931a0a
Mohammed Boutarbouch <mohamedboutarbouch48@gmail.com>
2026-04-03 23:52:51 +0100

    feat: hero disclaimer component added

---
+++ app/components/disclaimer.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Disclaimer() {
+  return (
+    <div className="relative p-12 opacity-90 rounded-4xl bg-primary/90">
+      <div className="absolute -top-5 -left-5 w-20">
+        <Image
+          src="/exclamation_mark.svg"
+          alt="Disclaimer"
+          width={40}
+          height={20}
+          className="w-full h-auto"
+        />
+      </div>
+      <p className="font-body text-white font-medium">You do not need to use a claims management company or solicitors, you can complain directly to your lender or to the Financial Ombudsman Service free of charge. The Financial Conduct Authority FCA is consulting on a potential redress scheme for specific motor-finance commission complaints. Lenders have until 4 December 2025 to respond to eligible complaints.</p>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 import { Container } from "./components/container";
+import Disclaimer from "./components/disclaimer";
 
 export default function Home() {
   return (
     <Container>
-      <p className="text-9xl underline text-primary">Hello</p>
+      <section className="grid grid-cols-1 md:grid-cols-2">
+
+        <div className="order-4 md:order-1">
+          <Disclaimer />
+        </div>
+
+        <div className="order-1 md:order-2">
+          {/* content */}
+        </div>
+
+        <div className="order-2 md:order-3">
+          {/* content */}
+        </div>
+
+        <div className="order-3 md:order-4 -mr-6 md:mr-0">
+          {/* content */}
+        </div>
+
+      </section>
     </Container>
   );
 }
