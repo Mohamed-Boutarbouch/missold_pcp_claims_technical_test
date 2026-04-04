b39f74d04bad7d1d2d763e251a8f406f314b4fa5
Mohammed Boutarbouch <mohamedboutarbouch48@gmail.com>
2026-04-04 12:00:29 +0100

    fix: optimize svgs using image nextjs component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,23 +5,23 @@ export function Header() {
   return (
     <Container>
       <div className="flex justify-between items-center">
-        <div className="w-36 md:w-48 lg:w-72">
+        <div className="relative w-36 md:w-48 lg:w-72 aspect-366/78">
           <Image
             src="/logo.svg"
             alt="Application logo"
-            width={366}
-            height={78}
-            className="w-full h-auto"
+            fill
+            className="object-contain"
+            loading="eager"
           />
         </div>
 
-        <div className="w-20 md:w-36">
+        <div className="relative w-20 md:w-36 aspect-3/1">
           <Image
             src="/secure.svg"
             alt="Secure badge"
-            width={150}
-            height={50}
-            className="w-full h-auto"
+            fill
+            className="object-contain"
+            loading="eager"
           />
         </div>
       </div>
